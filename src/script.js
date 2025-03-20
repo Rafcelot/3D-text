4cae8914908a8040c2bade62c3cce47c6ce5032d
Rafael Serna Arias <rafaserna58@hotmail.com>
2025-03-19 23:00:32 -0500

    Actualización del proyecto

--- src/script.js
+++ src/script.js
@@ -37,7 +37,7 @@ fontLoader.load(
 
         // Text
         const textGeometry = new TextGeometry(
-            'Hello Three.js',
+            'Camaronxxx',
             {
                 font: font,
                 size: 0.5,
