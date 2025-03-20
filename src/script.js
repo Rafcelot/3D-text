804a0f3bbffcc71263b043bd26eefa4ffcbc804d
Rafael Serna Arias <rafaserna58@hotmail.com>
2025-03-20 09:25:54 -0500

    marranito

--- src/script.js
+++ src/script.js
@@ -37,7 +37,7 @@ fontLoader.load(
 
         // Text
         const textGeometry = new TextGeometry(
-            'Camaronxxx',
+            'Marranito oloroso',
             {
                 font: font,
                 size: 0.5,
