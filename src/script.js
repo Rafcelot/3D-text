054377e5f099d2393c96567abad43b35f0d20efe
Rafael Serna Arias <rafaserna58@hotmail.com>
2025-03-20 09:29:04 -0500

    camaron

--- src/script.js
+++ src/script.js
@@ -37,7 +37,7 @@ fontLoader.load(
 
         // Text
         const textGeometry = new TextGeometry(
-            'Marranito oloroso',
+            'Camaron xxx',
             {
                 font: font,
                 size: 0.5,
